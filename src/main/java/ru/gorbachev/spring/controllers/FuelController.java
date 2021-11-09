ac12a85e904158cdbae5a37bd53f289eb7e02cee
OutdoorActivity <gorbachyov.fm@gmail.com>
2021-11-09 19:43:18 +0500

    заменил switch case на map

--- src/main/java/ru/gorbachev/spring/controllers/FuelController.java
+++ src/main/java/ru/gorbachev/spring/controllers/FuelController.java
@@ -1,34 +1,27 @@
 package ru.gorbachev.spring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.gorbachev.spring.service.DieselFuel;
 import ru.gorbachev.spring.service.Fuel;
-import ru.gorbachev.spring.service.PetrolFuel;
+import ru.gorbachev.spring.service.FuelMap;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/mvc/fuel")
 public class FuelController {
-
+    @Autowired
+    private FuelMap fuelMap;
 
     @FuelExceptionHandler
     @GetMapping("/check")
     public String greet(@RequestParam("type") String param, Model model) throws Exception {
-        Fuel fuel;
-        switch (param) {
-            case "diesel":
-                fuel = new DieselFuel();
-                break;
-            case "petrol":
-                fuel = new PetrolFuel();
-                break;
-            default:
-                throw new Exception();
-        }
-        model.addAttribute("message", fuel.powerUp());
+        model.addAttribute("message", fuelMap.getMap().get(param).powerUp());
         return "fuelType";
     }
 }
